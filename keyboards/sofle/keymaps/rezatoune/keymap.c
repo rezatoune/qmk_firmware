aa83b1c297b0f8bd45e7f24e16e7c6b7d3e2b4dd
Simon Brélivet <simon+github@brelivet.fr>
2021-10-08 00:03:55 +0200

    En cours !

--- keyboards/sofle/keymaps/rezatoune/keymap.c
+++ keyboards/sofle/keymaps/rezatoune/keymap.c
@@ -1,6 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "keymap_french.h"
 
+
+#define KC_CTPU RCTL_T(KC_PGUP) // PG haut si tapé, RCTRL si tenu
+#define KC_CTPD LCTL_T(KC_PGDN)
+
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _AZERTY,
@@ -11,16 +15,19 @@ enum sofle_layers {
 
 enum custom_keycodes {
     KC_AZERTY = SAFE_RANGE,
-    KC_LOWER = LT(_LOWER, KC_BSPACE),
-    KC_RAISE = LT(_RAISE, KC_ENTER),
+    //KC_LOWER = LT(_LOWER, KC_BSPACE),
+    //KC_RAISE = LT(_RAISE, KC_ENTER),
     KC_ADJUST,
     KC_PRVWD,
     KC_NXTWD,
     KC_LSTRT,
     KC_LEND,
-    KC_DLINE
+    KC_DLINE,
+    KC_VERR // LGUI + L : vérrouiller la session
 };
 
+#define KC_LOWER LT(_LOWER, KC_BSPACE)
+#define KC_RAISE LT(_RAISE, KC_ENTER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -41,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_AZERTY] = LAYOUT( \
   KC_ESC,   FR_AMPR, FR_EACU, FR_DQUO, FR_QUOT, FR_LPRN,                   FR_MINS, FR_EGRV, FR_UNDS, FR_CCED, FR_AGRV, FR_RPRN, \
-  KC_TAB,   FR_A,    FR_Z,    FR_E,    FR_R,    FR_T,                      FR_Y,    FR_U,    FR_I,    FR_O,    FR_P,    FR_ASTR, \
-  XXXXXXX,  FR_Q,    FR_S,    FR_D,    FR_F,    FR_G,                      FR_H,    FR_J,    FR_K,    FR_L,    FR_M,    FR_UGRV, \
-  FR_EQL,   FR_W,    FR_X,    FR_C,    FR_V,    FR_B,  KC_MUTE,     XXXXXXX,FR_N,   FR_COMM, FR_SCLN, FR_COLN, FR_EXLM, FR_LABK,\
-                  KC_LALT,KC_LALT,KC_CTRL, KC_LOWER, KC_LSFT,      KC_SPC,  KC_RAISE, KC_TODO, KC_RGUI, FR_CIRC \
+  XXXXXXX,  FR_A,    FR_Z,    FR_E,    FR_R,    FR_T,                      FR_Y,    FR_U,    FR_I,    FR_O,    FR_P,    FR_ASTR, \
+  KC_TAB,   FR_Q,    FR_S,    FR_D,    FR_F,    FR_G,                      FR_H,    FR_J,    FR_K,    FR_L,    FR_M,    FR_UGRV, \
+  FR_EQL, FR_W,    FR_X,    FR_C,    FR_V,    FR_B,  KC_VERR,     XXXXXXX,FR_N,   FR_COMM, FR_SCLN, FR_COLN, FR_EXLM, FR_DLR,\
+                  KC_LABK,KC_LALT,KC_CTPD, KC_LOWER, KC_LSFT,      KC_SPC,  KC_RAISE, KC_CTPU, KC_RALT, FR_CIRC \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -62,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,\
-  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12, \
+  _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12, \
   _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
   _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______, \
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______\
@@ -71,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Esc  | Ins  | Pscr | Menu |      |      |                    |      | PWrd |  Up  | NWrd | DLine| Bspc |
+ * | Esc  | Ins  | Pscr | Menu |ArDfil|      |                    |      | PWrd |  Up  | NWrd | DLine| Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  | LAt  | LCtl |LShift|      | Caps |-------.    ,-------|      | Left | Down | Rigth|  Del | Bspc |
  * |------+------+------+------+------+------|  MUTE  |    |       |------+------+------+------+------+------|
@@ -83,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT( \
   _______, _______ , _______ , _______ , _______ , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______, \
-  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,KC_DLINE, KC_BSPC, \
+  _______,  KC_INS,  KC_PSCR,   KC_APP,  KC_LSCR, XXXXXXX,                        KC_PGUP, KC_PRVWD,   KC_UP, KC_NXTWD,KC_DLINE, KC_BSPC, \
   _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC, \
   _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______, \
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______ \
@@ -111,6 +118,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+uint32_t layer_state_set_user(uint32_t state) {
+    return update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -119,7 +129,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 set_single_persistent_default_layer(_AZERTY);
             }
             return false;
-        case KC_LOWER:
+       /* case KC_LOWER:
             if (record->event.pressed) {
                 layer_on(_LOWER);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
@@ -127,7 +137,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 layer_off(_LOWER);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
             }
-            return false;
+            //return false;
         case KC_RAISE:
             if (record->event.pressed) {
                 layer_on(_RAISE);
@@ -136,14 +146,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 layer_off(_RAISE);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
             }
-            return false;
+            //return false;
         case KC_ADJUST:
             if (record->event.pressed) {
                 layer_on(_ADJUST);
             } else {
                 layer_off(_ADJUST);
             }
-            return false;
+            return false; */
         case KC_PRVWD:
             if (record->event.pressed) {
                 if (keymap_config.swap_lctl_lgui) {
@@ -262,6 +272,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 unregister_mods(mod_config(MOD_LCTL));
                 unregister_code(KC_Z);
+            }
+            return false;
+        case KC_VERR:
+	    if (record->event.pressed) {
+                register_mods(mod_config(MOD_LGUI));
+                register_code(KC_L);
+            } else {
+                unregister_mods(mod_config(MOD_LGUI));
+                unregister_code(KC_L);
             }
             return false;
     }
